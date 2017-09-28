81ca0f1adc272d3753a0d36a61a9c13f7ecb4afc
Giuseppe Volpicella <g.volpicella@earnaway.com>
2017-09-28 20:03:40 +0100

    Removed dummy expense data

--- src/app/app.js
+++ src/app/app.js
@@ -14,23 +14,6 @@ import 'react-dates/lib/css/_datepicker.css';
 // setup store
 const store = configureStore();
 
-// log store state
-store.subscribe(() => {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    console.log(visibleExpenses);
-})
-
-// add two expenses
-store.dispatch(addExpense({ description : 'Water bill', amount : 4500 }));
-store.dispatch(addExpense({ description : 'Gas bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description : 'Rent', amount : 109500 }));
-
-
-// set text filter on expenses
-//store.dispatch(setTextFilter('bill'));
-//store.dispatch(setTextFilter('water'));
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
